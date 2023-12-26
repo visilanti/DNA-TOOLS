4ac5c1dac4c3daefe65d8bf24d63f88f6b4b7eae
silvianurrobianti <silvianurrobianti@gmail.com>
2023-12-26 15:36:01 +0700

    first commit

---
+++ DNAToolkit.py
@@ -0,0 +1,14 @@
+Nucleotides = ["A", "C", "G", "T"]
+
+def validateSeq(dna_seq):
+    tmpseq = dna_seq.upper()
+    for nuc in tmpseq:
+        if nuc not in Nucleotides:
+            return False
+    return True
+
+def countNucFrequency(seq):
+    tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for nuc in seq:
+        tmpFreqDict[nuc] += 1
+    return tmpFreqDict
